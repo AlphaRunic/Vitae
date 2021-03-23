b807d1ea1340ff429d6b22893d7738fd8a9fffc8
AlphaRunic <roleyspersonal@gmail.com>
2021-03-23 00:27:40 -0700

    Added syntax types, tokens, and a lexer

---
+++ vc/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace vc {
+    class Program {
+        static void Main(string[] args) {
+            while(true) {
+                Console.Write("vitae >> ");
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    return;
+
+                // if (line == "1 + 2 * 3")
+                //     Console.WriteLine("7");
+                // else
+                //     Console.WriteLine("Error: Invalid expression.");
+            }
+        }
+    }
+
+    class Token {
+
+        public string _type;
+        public id
+
+        public Token(string type, id value) {
+            _type = type;
+            _value = value;
+        }
+    }
+
+    class Lexer {
+
+        private readonly string _text;
+        private int _pos;
+
+        public Lexer(string text) {
+            _text = text;
+        }
+
+        public Token NextToken() {
+
+        }
+    }
+}
