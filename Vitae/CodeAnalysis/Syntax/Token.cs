245703e46e6b4f1c7c58dfcd9bed72e174cd33e2
AlphaRunic <roleyspersonal@gmail.com>
2021-03-26 21:43:37 +0100

    Add SyntaxNode.Span

--- Vitae/CodeAnalysis/Syntax/Token.cs
+++ Vitae/CodeAnalysis/Syntax/Token.cs
@@ -3,11 +3,11 @@ using System.Linq;
 
 namespace Vitae.CodeAnalysis.Syntax {
     public sealed class Token : SyntaxNode {
+        public override  TextSpan Span => new TextSpan(Position, Text.Length);
         public override SyntaxType Type { get; }
         public int Position { get; }
         public string Text { get; }
         public object Value { get; }
-        public TextSpan Span => new TextSpan(Position, Text.Length);
 
         public Token(SyntaxType type, int pos, string text, object value) {
             Type = type;
