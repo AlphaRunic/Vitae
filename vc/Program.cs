7d04d30cb27060d0e46d0113c49d42c5188145f5
AlphaRunic <roleyspersonal@gmail.com>
2021-03-29 20:01:35 +0100

    Add concept of lines

--- vc/Program.cs
+++ vc/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Text;
 using Vitae.CodeAnalysis;
 using Vitae.CodeAnalysis.Binding;
 using Vitae.CodeAnalysis.Syntax;
+using Vitae.CodeAnalysis.Text;
 
 namespace Vitae
 {
@@ -12,34 +13,46 @@ namespace Vitae
     {
         private static void Main(string[] args)
         {
-            bool showTree = false;
+            var showTree = false;
             var variables = new Dictionary<VariableSymbol, object>();
+            var textBuilder = new StringBuilder();
 
             while (true)
             {
-                Console.Write("Vitae >> ");
-                var line = Console.ReadLine();
-                if (string.IsNullOrEmpty(line))
-                    return;
+                if (textBuilder.Length == 0)
+                    Console.Write("Vitae >> ");
+                else
+                    Console.Write("| ");
 
-                if (line == "#showTrees")
-                {
-                    showTree = !showTree;
-                    Console.WriteLine(showTree ? "Showing parse trees." : "Not showing parse trees.");
-                    continue;
-                }
-                else if (line == "#cls")
-                {
-                    Console.Clear();
-                    continue;
-                }
+                var input = Console.ReadLine();
+                var isBlank = string.IsNullOrEmpty(input);
 
-                var syntaxTree = SyntaxTree.Parse(line);
+                if (textBuilder.Length == 0)
+                {
+                    if (isBlank)
+                        break;
+                    else if (input == "#showTrees")
+                    {
+                        showTree = !showTree;
+                        Console.WriteLine(showTree ? "Showing parse trees." : "Not showing parse trees.");
+                        continue;
+                    }
+                    else if (input == "#cls")
+                    {
+                        Console.Clear();
+                        continue;
+                    }
+                }
+                
+                textBuilder.AppendLine(input);
+                var text = textBuilder.ToString();
+                var syntaxTree = SyntaxTree.Parse(text);
                 var compilation = new Compilation(syntaxTree);
                 var result = compilation.Evaluate(variables);
 
-                var diagnostics = result.Diagnostics;
-                
+                if (!isBlank && syntaxTree.Diagnostics.Any())
+                    continue;
+
                 if (showTree)
                 {
                     Console.ForegroundColor = ConsoleColor.Cyan;
@@ -47,15 +60,18 @@ namespace Vitae
                     Console.ResetColor();
                 }
 
-                if (diagnostics.Any())
+                if (!result.Diagnostics.Any())
                 {
-                    var text = syntaxTree.Text;
-
-                    foreach (var diagnostic in diagnostics)
+                    Console.WriteLine(result.Value);
+                }
+                else
+                {
+                    foreach (var diagnostic in result.Diagnostics)
                     {
-                        var lineIndex = text.GetLineIndex(diagnostic.Span.Start);
+                        var lineIndex = syntaxTree.Text.GetLineIndex(diagnostic.Span.Start);
                         var lineNumber = lineIndex + 1;
-                        var character = diagnostic.Span.Start - text.Lines[lineIndex].Start + 1;
+                        var line = syntaxTree.Text.Lines[lineIndex];
+                        var character = diagnostic.Span.Start - line.Start + 1;
                         Console.WriteLine();
 
                         Console.ForegroundColor = ConsoleColor.DarkRed;
@@ -63,9 +79,12 @@ namespace Vitae
                         Console.WriteLine(diagnostic);
                         Console.ResetColor();
 
-                        string prefix = line.Substring(0, diagnostic.Span.Start);
-                        string error = line.Substring(diagnostic.Span.Start, diagnostic.Span.Length);
-                        string suffix = line.Substring(diagnostic.Span.End);
+                        TextSpan prefixSpan = TextSpan.FromBounds(line.Start, diagnostic.Span.Start);
+                        TextSpan suffixSpan = TextSpan.FromBounds(diagnostic.Span.End, line.End);
+
+                        string prefix = syntaxTree.Text.ToString(prefixSpan);
+                        string error = syntaxTree.Text.ToString(diagnostic.Span);
+                        string suffix = syntaxTree.Text.ToString(suffixSpan);
 
                         Console.Write("    ");
                         Console.Write(prefix);
@@ -80,10 +99,8 @@ namespace Vitae
 
                     Console.WriteLine();
                 }
-                else
-                {
-                    Console.WriteLine(result.Value);
-                }
+
+                textBuilder.Clear();
             }
         }
     }
